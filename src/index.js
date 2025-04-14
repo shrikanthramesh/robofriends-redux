74acf8b12c97d019923354ecfb7ca2d43b97c915
Shrikanth Eligeti <shrikanth.eligeti@kanishkasoftware.com>
2025-04-14 23:49:30 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,40 +1,36 @@
-import React from 'react';
+import React from "react";
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
 //import thunkMiddleware from 'redux-thunk';
 //NEW way of importing redux-thunk:
-import { thunk } from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import 'tachyons';
+import { thunk } from "redux-thunk";
+import { createLogger } from "redux-logger";
+import "tachyons";
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { requestRobots, searchRobots } from "./reducers";
+import "./index.css";
 
+const logger = createLogger();
 
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import { requestRobots, searchRobots } from './reducers'
+const rootReducers = combineReducers({ requestRobots, searchRobots });
+const store = createStore(rootReducers, applyMiddleware(thunk, logger));
 
-import './index.css';
-
-const logger = createLogger() 
-
-const rootReducers = combineReducers({requestRobots, searchRobots})
-
-const store = createStore(rootReducers, applyMiddleware(thunk, logger))
-
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
-<Provider store={store}>
-  <App/>
-</Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
+registerServiceWorker();
+// The old way to render the app:
 // ReactDOM.render(
 //   <Provider store={store}>
 //     <App/>
 //   </Provider>,
 //   document.getElementById('root')
 // );
-registerServiceWorker();
-
