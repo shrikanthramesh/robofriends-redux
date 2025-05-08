98ed8f2c4419789b4ab449dbe0773b4a54d6d685
Shrikanth Eligeti <shrikanth.eligeti@kanishkasoftware.com>
2025-05-08 16:19:36 +0530

    registered service worker

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // The old way to render the app:
 // ReactDOM.render(
